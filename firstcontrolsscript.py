10ab1f03d50127c27b6cfa6bedc234bf7fa67142
sherlock-11jb <bpololo11@gamil.com>
2024-12-02 18:27:21 -0500

    copias de seguridad

---
+++ firstcontrolsscript.py
@@ -0,0 +1,14 @@
+print ("what is your name?")
+
+name=input()
+
+print("what is your password?")
+
+password=input ()
+
+if name == "johannes" :
+	print("hello johannes")
+	if password == "password" :
+		print("access granted")
+	else:
+		print("wrong password")
---
+++ myfirstscript.py
@@ -0,0 +1,18 @@
+# this my first python program
+
+print ("hello Hacker-Arise!")
+print ("what is your name?") # we are asking for the user's name 
+
+myname = input() # this accepts the ketboard input from the user and puts it into a variable called myname
+
+print ("its good to meet you, " + myname) #greeting to the user using keyboard input
+
+print ("the length of your name is: ")
+
+print (len(myname)) #this prints the number of characters in the users name
+
+print("what is your age?")
+
+myage = input()
+
+print(myname + " is " + myage + " years old")
